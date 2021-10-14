7e18bcffe4ad8304394902f8c0e6eda4835580af
KaWa <Karollo47@gmail.com>
2021-10-14 20:40:52 +0200

    initial commit

---
+++ src/pl/astralkarolwaw/Main.java
@@ -0,0 +1,15 @@
+package pl.astralkarolwaw;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
