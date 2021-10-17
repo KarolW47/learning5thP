b6926544a845d58ba476a9f082f10987c18cbbfc
KaWa <Karollo47@gmail.com>
2021-10-17 20:47:05 +0200

    small fixes

--- src/pl/astralkarolwaw/Main.java
+++ src/pl/astralkarolwaw/Main.java
@@ -6,38 +6,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scannerForInts = new Scanner(System.in);
+        Scanner scannerForStrings = new Scanner(System.in);
         boolean isStillRunning = true;
-        int typedNumber1St;
-        int typedNumber2Nd;
+        double typedNumber1St;
+        double typedNumber2Nd;
         int chosenOperation;
         String chosenExitOption;
 
         System.out.println("Welcome in kinda simple calculator :)");
         while (isStillRunning){
             System.out.println("Type first number:");
-            typedNumber1St = scanner.nextInt();
+            typedNumber1St = scannerForInts.nextInt();
 
             System.out.println("Press 1 to +");
             System.out.println("Press 2 to -");
             System.out.println("Press 3 to *");
             System.out.println("Press 4 to /");
             System.out.println("Chose operation:");
-            chosenOperation = scanner.nextInt();
+            chosenOperation = scannerForInts.nextInt();
 
             System.out.println("Type second number:");
-            typedNumber2Nd = scanner.nextInt();
+            typedNumber2Nd = scannerForInts.nextInt();
 
             switch (chosenOperation){
-                case 1 -> System.out.println("Restult = " + typedNumber1St+typedNumber2Nd);
+                case 1 -> System.out.println("Restult = " + (typedNumber1St+typedNumber2Nd));
                 case 2 -> System.out.println("Restult = " + (typedNumber1St-typedNumber2Nd));
-                case 3 -> System.out.println("Restult = " + typedNumber1St*typedNumber2Nd);
-                case 4 -> System.out.println("Restult = " + typedNumber1St/typedNumber2Nd);
+                case 3 -> System.out.println("Restult = " + (typedNumber1St*typedNumber2Nd));
+                case 4 -> System.out.println("Restult = " + (typedNumber1St/typedNumber2Nd));
 
             }
             System.out.println("Next operation? y/n");
-            Scanner scanner1 = new Scanner(System.in);
-            chosenExitOption = scanner1.nextLine();
+            chosenExitOption = scannerForStrings.nextLine();
             if (chosenExitOption.equals("n")){
                 isStillRunning = false;
             }
